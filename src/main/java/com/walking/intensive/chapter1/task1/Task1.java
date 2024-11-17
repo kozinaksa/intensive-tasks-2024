ba5d0405382f5970e970593ad3d161c56c04f992
Galina Moroz <kozinaksa@gmail.com>
2024-11-18 02:04:16 +0300

    task1 fix solution

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,7 +15,7 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = -10;
 
         System.out.println(getAgeString(age));
     }
@@ -24,6 +24,10 @@ public class Task1 {
 
         int lastDigit = age % 10;
         int penultDigit = age / 10 % 10;
+
+        if (age < 0) {
+            return "Некорректный ввод";
+        }
 
         switch (lastDigit) {
             case 1:
