d1199340cc5d753444ad75efdad281101dca92e8
Galina Moroz <kozinaksa@gmail.com>
2024-11-18 21:03:01 +0300

    task4 solved

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -34,8 +34,26 @@ public class Task4 {
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a == 0 && c == 0) {
+            return  "Бесконечное множество решений.";
+        } else if (a == 0 && b == 0) {
+            return "Количество решений: 0.";
+        } else if (a == 0) {
+            return "Количество решений: 1. Корень: " + -c / b;
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        double d = Math.pow(b, 2) - 4 * a * c;
+        if (d < 0) {
+            return "Количество решений: 0.";
+        }
+
+        double x1 = (-b + Math.pow(d, 0.5)) / 2 * a;
+        if (d == 0) {
+            return "Количество решений: 1. Корень: " + x1;
+        }
+
+        double x2 = (-b - Math.pow(d, 0.5)) / 2 * a;
+        String roots = x1 < x2 ? x1 + ";" + x2 : x2 + ";" + x1;
+        return "Количество решений: 2. Корни: " + roots;
     }
 }
