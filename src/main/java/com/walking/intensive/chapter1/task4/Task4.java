85a8dcdda772febe6caac4610a6202d845ea7df7
Galina Moroz <kozinaksa@gmail.com>
2024-11-19 01:27:46 +0300

    task4 improve solution

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -36,23 +36,25 @@ public class Task4 {
     static String solveEquation(double a, double b, double c) {
         if (a == 0 && c == 0) {
             return  "Бесконечное множество решений.";
-        } else if (a == 0 && b == 0) {
+        }
+        if (a == 0 && b == 0) {
             return "Количество решений: 0.";
-        } else if (a == 0) {
+        }
+        if (a == 0) {
             return "Количество решений: 1. Корень: " + -c / b;
         }
 
-        double d = Math.pow(b, 2) - 4 * a * c;
-        if (d < 0) {
+        double discriminant = Math.pow(b, 2) - 4 * a * c;
+        if (discriminant < 0) {
             return "Количество решений: 0.";
         }
 
-        double x1 = (-b + Math.pow(d, 0.5)) / 2 * a;
-        if (d == 0) {
+        double x1 = (-b + Math.sqrt(discriminant)) / 2 * a;
+        if (discriminant == 0) {
             return "Количество решений: 1. Корень: " + x1;
         }
 
-        double x2 = (-b - Math.pow(d, 0.5)) / 2 * a;
+        double x2 = (-b - Math.sqrt(discriminant)) / 2 * a;
         String roots = x1 < x2 ? x1 + ";" + x2 : x2 + ";" + x1;
         return "Количество решений: 2. Корни: " + roots;
     }
