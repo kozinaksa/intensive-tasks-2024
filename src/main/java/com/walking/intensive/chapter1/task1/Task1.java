ba4bbd4e514483828a9df8eb902146e3029f43c1
Galina Moroz <kozinaksa@gmail.com>
2024-11-18 17:57:41 +0300

    task1 change solution way (from 'switch' to 'if')

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -22,39 +22,21 @@ public class Task1 {
 
     static String getAgeString(int age) {
 
-        int lastDigit = age % 10;
-        int penultDigit = age / 10 % 10;
-
         if (age < 0) {
             return "Некорректный ввод";
         }
 
-        switch (lastDigit) {
-            case 1:
-                if (penultDigit == 1) {
-                    return "Вам " + age + " лет";
-                }
-                else {
-                    return "Вам " + age + " год";
-                }
-            case 2:
-            case 3:
-            case 4:
-                if (penultDigit == 1) {
-                    return "Вам " + age + " лет";
-                }
-                else {
-                    return "Вам " + age + " года";
-                }
-            case 0:
-            case 5:
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-                return "Вам " + age + " лет";
-            default:
-                return "Некорректный ввод";
+        int lastDigit = age % 10;
+        int penultDigit = age / 10 % 10;
+        String yearText = " лет";
+
+        if (lastDigit == 1 && penultDigit != 1) {
+            yearText = " год";
         }
+        else if (lastDigit >= 2 && lastDigit <= 4 && penultDigit != 1) {
+            yearText = " года";
+        }
+
+        return "Вам " + age + yearText;
     }
 }
