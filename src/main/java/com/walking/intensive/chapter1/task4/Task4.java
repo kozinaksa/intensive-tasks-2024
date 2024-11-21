1d73858a6e491ada16ea1646ad6f5ad3571e8d1b
Galina Moroz <kozinaksa@gmail.com>
2024-11-21 18:20:28 +0300

    task4 added blank lines around "if" blocks

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -37,19 +37,23 @@ public class Task4 {
         if (a == 0 && c == 0) {
             return  "Бесконечное множество решений.";
         }
+
         if (a == 0 && b == 0) {
             return "Количество решений: 0.";
         }
+
         if (a == 0) {
             return "Количество решений: 1. Корень: " + -c / b;
         }
 
         double discriminant = Math.pow(b, 2) - 4 * a * c;
+
         if (discriminant < 0) {
             return "Количество решений: 0.";
         }
 
         double x1 = (-b + Math.sqrt(discriminant)) / 2 * a;
+
         if (discriminant == 0) {
             return "Количество решений: 1. Корень: " + x1;
         }
