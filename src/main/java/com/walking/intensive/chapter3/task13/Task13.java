4b060f6c796101a82ea0902093195a6d266ff980
Galina Moroz <kozinaksa@gmail.com>
2024-12-08 20:31:22 +0300

    task13 solved

--- src/main/java/com/walking/intensive/chapter3/task13/Task13.java
+++ src/main/java/com/walking/intensive/chapter3/task13/Task13.java
@@ -50,10 +50,37 @@ package com.walking.intensive.chapter3.task13;
 public class Task13 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
+        System.out.println(getStepsCount(new int[]{1, 1, 1, 4, 2, 3}, 4));
     }
 
     static int getStepsCount(int[] plants, int wateringCanVolume) {
-        // Ваш код
-        return 0;
+        if (wateringCanVolume < 1) {
+            return -1;
+        }
+
+        if (plants == null || plants.length < 1) {
+            return 0;
+        }
+
+        for (int plant : plants) {
+            if (plant < 1) {
+                return -1;
+            }
+        }
+
+        int waterVolume = wateringCanVolume;
+        int steps = 0;
+
+        for (int i = 0; i < plants.length; i++) {
+            if (waterVolume < plants[i]) {
+                steps += i * 2;
+                waterVolume = wateringCanVolume;
+            }
+
+            steps++;
+            waterVolume -= plants[i];
+        }
+
+        return steps;
     }
 }
